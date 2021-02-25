0350386a173877109b119fe8970e11491c7f7253
Jack <jackacbuchanan@gmail.com>
2021-02-25 20:57:43 +0000

    Anything

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>© JackBnB 2021 - No rights reserved, this is a demo! </p>
+            <p>Privacy . Terms . SiteMap . Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
